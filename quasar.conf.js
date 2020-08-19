cd4a8faabd11b34c977fb7b14a27bd6c32f47944
ibomac <aykilicpanel@gmail.com>
2020-08-19 21:58:31 +0300

    21:58

--- quasar.conf.js
+++ quasar.conf.js
@@ -94,6 +94,7 @@ module.exports = function (ctx) {
       gzip: false,
       analyze: false,
       publicPath:ctx.dev
+      // ? '' :  'http://emosetekstil.com.tr/',
       ? '' :  'http://emosetekstil.com.tr/',
       env: ctx.dev
     ? { // so on dev we'll have
