5a22c98903c6726c03ef1a21d73c1427e4cb67bf
ibomac <aykilicpanel@gmail.com>
2020-09-27 02:13:42 +0300

    02:13

--- quasar.conf.js
+++ quasar.conf.js
@@ -106,7 +106,7 @@ module.exports = function (ctx) {
 
       API: JSON.stringify('http://127.0.0.1'),
       
-      WAPI:JSON.stringify('ws://localhost'),
+      WAPI:JSON.stringify('wss://localhost'),
       // JWT_SECRET:JSON.stringify('emose2-app3-jwt4')
     },
       // Options below are automatically set depending on the env, set them if you want to override
